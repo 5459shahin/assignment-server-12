a6e4be0b333b26dd5a8805bf2cb07778edbc41f8
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-05-28 23:49:04 +0800

    add admin

--- index.js
+++ index.js
@@ -16,56 +16,108 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try {
-        await client.connect();
-        // console.log('database connected')
-        const serviceCollection = client.db('assignment').collection('services');
+  try {
+    await client.connect();
+    // console.log('database connected')
+    const serviceCollection = client.db('assignment').collection('services');
+    const userCollection = client.db('assignment').collection("users");
+    const orderCollection = client.db('assignment').collection("order");
+
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+
+    });
+
+    //update
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body;
+      const result = await serviceCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    //purchase
+    app.get('/purchase/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      console.log('ok');
+      res.send(result);
+      // console.log(req);
+    });
+
+    // store new user email
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+
+    });
+
+    // my orders
+    app.get('/order/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    /* 
     
+      // get a specific service tool by id
+app.get('/products/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id:ObjectId(id)};
+  const result = await toolsCollection.findOne(query);
+  res.send(result);
+})
+// my orders
+app.get('/order/:email', async (req, res)=>{
+  const email = req.params.email;
+  const query ={email:email}
+  const cursor = orderCollection.find(query);
+  const result = await cursor.toArray()
+  res.send(result)
+})
+ 
+    */
 
-        app.get('/service', async (req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
 
-        });
 
-        //purchase
-        app.get('/purchase/:id', async(req, res) =>{
-            const id = req.params.id;
-            const query = {_id:ObjectId(id)};
-            const result = await serviceCollection.findOne(query);
-            console.log('ok');
-            res.send(result);
-            // console.log(req);
-          })
 
-        app.post('/booking', async (req, res) => {
-            const booking = req.body;
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result);
-        })
-        /***
-         * api naming convention
-         * app.get('/booking.) // get all booking
-         * app.get('/booking/:id') // get a specific booking
-         * app.post('/booking') // add a new booking
-         * app.patch('/booking/:id')//specific
-         * app.delete('/booking/:id')//specific
-         */
 
-    }
-    finally {
 
-    }
+    /***
+     * api naming convention
+     * app.get('/booking.) // get all booking
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id')//specific
+     * app.delete('/booking/:id')//specific
+     */
+
+  }
+  finally {
+
+  }
 }
 
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('get START ASSIGNMENT 12')
+  res.send('get START ASSIGNMENT 12')
 })
 
 app.listen(port, () => {
-    console.log(`assignment app listening on port ${port}`)
+  console.log(`assignment app listening on port ${port}`)
 })
